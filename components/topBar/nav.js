45b1a59cec10f57968d872fbfe7688c3a775cf8c
Anastasiia Kiias <anastasiiakiias@gmail.com>
2023-12-14 12:46:40 +0100

    feat: adding a new way to render templates

---
+++ components/topBar/nav.js
@@ -0,0 +1,20 @@
+const Nav = () => {
+    return /*html*/`
+    <div class="navbar">
+    <div class="logo">
+      <a href="your-homepage-url">
+        <img src="../../assets/logo.png" alt="Your Logo" />
+      </a>
+    </div>
+    <div class="middle">
+      <a class="nav-link" href="#">Home</a>
+      <a class="nav-link" href="#about">About</a>
+      <a class="nav-link" href="#resume">Resume</a>
+    </div>
+    <div class="right">
+      <a class="primary-btn nav-link" href="#contact">Contact</a>
+    </div>
+  </div>`;
+};
+
+document.getElementById("nav").innerHTML = Nav();
