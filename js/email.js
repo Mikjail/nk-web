ece9328c1b03697dfe165bac4f8482655f910fed
MikjailS <mikjailsalazar@gmail.com>
2024-01-03 19:42:16 +0100

    fix: changing gha

--- js/email.js
+++ js/email.js
@@ -4,7 +4,6 @@ window.onload = async function () {
     const form = e.target;
     const formData = new FormData(form);
     const { name, email, message } = Object.fromEntries(formData.entries());
-    console.log(name, email, message);
     emailjs.send(
       import.meta.env.VITE_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
