90d3ff5bb6a58e057752b100d90c197699ad8ae5
MikjailS <mikjailsalazar@gmail.com>
2024-01-04 15:54:24 +0100

    fix: adding public key

--- js/email.js
+++ js/email.js
@@ -1,17 +1,23 @@
 const EMAILJS_TEMPLATE_ID = "template_0g5u8p4";
 const EMAILJS_SERVICE_ID = "service_zd2m69w";
+const EMAILJS_PUBLIC_KEY = "qQIdKbKNoyaz0Z5rb";
 window.onload = async function () {
   const onSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
     const formData = new FormData(form);
     const { name, email, message } = Object.fromEntries(formData.entries());
-    emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, {
-      from_name: name,
-      to_name: "Anastasiia",
-      from_email: email,
-      message: message,
-    });
+    emailjs.send(
+      EMAILJS_SERVICE_ID,
+      EMAILJS_TEMPLATE_ID,
+      {
+        from_name: name,
+        to_name: "Anastasiia",
+        from_email: email,
+        message: message,
+      },
+      EMAILJS_PUBLIC_KEY
+    );
 
     form.reset();
   };
