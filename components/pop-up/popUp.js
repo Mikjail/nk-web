2bcc0efcfee29e3210662e37b88d5c232e70ee6a
Anastasiia Kiias <anastasiiakiias@gmail.com>
2024-01-18 19:08:11 +0100

    feat: adding pop-up message after sending contact email

---
+++ components/pop-up/popUp.js
@@ -0,0 +1,11 @@
+export const PopUp = () =>  /*html*/ `
+     <div class="pop-up__header">
+        Thank you!
+     </div>
+     <div class="pop-up__body">
+        Your message was successfully sent!
+     </div>
+     <div class="pop-up__footer">
+        <button id="pop-up__close" class="pop-up__close-btn">Close</button>
+     </div>
+`;
