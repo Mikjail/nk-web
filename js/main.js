fcef5f7ad23e176821fe33e7925f5713e98d8fe2
AnastasiiaKiias <153505200+AnastasiiaKiias@users.noreply.github.com>
2024-01-26 10:16:57 +0100

    feat: popup for case studies on mobile (#21)
    * minor fixes on case studies
    
    * adding popup message for case studies on mobile view

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import { Nav } from "../components/topBar/nav.js";
 import { Footer } from "../components/footer/footer.js";
 import { Modal } from "../components/modal/modal.js";
-import { PopUp } from "../components/pop-up/popUp.js";
+import { PopUpCase } from "../components/pop-up/popUpCase.js";
 
 
 document.getElementById("nav").innerHTML = Nav();
 document.getElementById("footer").innerHTML = Footer();
-document.getElementById("mobile-dialog").innerHTML = PopUp();
+document.getElementById("mobile-dialog").innerHTML = PopUpCase();
 if(!window.matchMedia("(max-width: 430px)").matches) {
     document.getElementById("dialog-form").innerHTML = Modal();
 }
